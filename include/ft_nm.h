fee112e7739668f195692bf762434dc5fbc28b60
Hera <tennyhera.r@gmail.com>
2025-02-24 23:08:34 +0100

    Ajout du fichier ft_nm.h

--- include/ft_nm.h
+++ include/ft_nm.h
@@ -3,11 +3,22 @@
 
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <ar.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <elf.h>
 #include <sys/stat.h>
 #include <libft.h>
+
+
+
+typedef struct s_symbols
+{
+    uint64_t    st_value;
+    char        symbol_type;
+    char        *symbol_name;
+}               t_symbols;
+
 
 #endif
