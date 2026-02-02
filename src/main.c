3b1a196e9f83cd117906cdb89e1d19da4895e7da
Hera.r <tennyhera.r@gmail.com>
2026-02-01 22:43:29 -0500

    Clean up and refactor main function

--- src/main.c
+++ src/main.c
@@ -1,95 +1,60 @@
 #include "../include/ft_nm.h"
 
-int main(int argc, char *argv[]) {
 
-	const char	*filename;
-	if (argc < 2)
-		filename = "a.out";
-	else
-		filename = argv[1];
+static int	process_file(void *file_mem, struct stat *st, char *filename)
+{
+	Elf64_Ehdr	*ehdr;
 
-	int fd = open(filename, O_RDONLY);
-	if (fd < 0) {
-		perror("open");
-		return 1;
-	}
-
-	struct stat st;
-	if (fstat(fd, &st) < 0) {
-		perror("fstat");
-		return 1;
-	}
-
-	void *file_mem = mmap(NULL, st.st_size , PROT_READ, MAP_PRIVATE, fd, 0);
-	if (file_mem == MAP_FAILED) {
-		perror("mmap");
-		return 1;
-	}
-
-	Elf64_Ehdr *ehdr = (Elf64_Ehdr *)file_mem;
-	Elf64_Shdr *section_header = (Elf64_Shdr *)(file_mem + ehdr->e_shoff);
-
-	// t_symbols *symbol_tab;
-	int nb_symbols = 0;
-
-	if (ft_strncmp(file_mem, ARMAG, SARMAG) == 0) 
+	ehdr = (Elf64_Ehdr *)file_mem;
+	if (ft_strncmp(file_mem, ARMAG, SARMAG) == 0)
 	{
-		char	*end = (char *)file_mem + st.st_size;
-		if (handle_archive(file_mem, end, filename) == 1)
-			return 0;
+		if (handle_archive(file_mem, (char *)file_mem + st->st_size, filename) == 1)
+			return (1);
 	}
-	else if (check_elf_magic(ehdr) == 0) {
-		Elf64_Shdr *symtab = NULL;
-		if (!(symtab = find_symtab(ehdr, section_header)))
-		{
-			ft_nm_error(filename, "no symbols");
-			return 1;
-		}
-		Elf64_Sym * symbo = (Elf64_Sym *)(file_mem + symtab->sh_offset);
-
-		nb_symbols = symtab->sh_size / sizeof(Elf64_Sym);
-
-		Elf64_Shdr *strtab = &section_header[symtab->sh_link];
-
-		for (int i = 0; i < nb_symbols; i++) {
-			if (symbo[i].st_name == 0 || ELF64_ST_TYPE(symbo[i].st_info) == STT_FILE) continue;
-			char c = get_symbol_type(&symbo[i], section_header);
-			char *symbol_name = (char *)(file_mem + strtab->sh_offset + symbo[i].st_name);
-			if (symbo[i].st_value == 0) {
-				ft_putstr_fd("                 ", STDOUT_FILENO);
-				ft_putchar_fd(c, STDOUT_FILENO);
-				ft_putstr_fd(" ", STDOUT_FILENO);
-				ft_putstr_fd(symbol_name, STDOUT_FILENO);
-				ft_putchar_fd('\n', STDOUT_FILENO);
-			} else {
-				ft_print_hexa(symbo[i].st_value);
-				ft_putstr_fd(" ", STDOUT_FILENO);
-				ft_putchar_fd(c, STDOUT_FILENO);
-				ft_putstr_fd(" ", STDOUT_FILENO);
-				ft_putstr_fd(symbol_name, STDOUT_FILENO);
-				ft_putchar_fd('\n', STDOUT_FILENO);
-			}
-		}
+	else if (check_elf_magic(ehdr) == 0)
+	{
+		if (handle_elf_file(file_mem, filename) == 1)
+			return (1);
 	}
-	else {
+	else
+	{
 		ft_nm_error(filename, "file format not recognized");
-		return 1;
+		return (1);
 	}
-		
-	return 0;
+	return (0);
 }
 
 
+int	main(int argc, char *argv[])
+{
+	const char	*filename;
+	int			fd;
+	struct stat	st;
+	void		*file_mem;
+	int			ret;
 
-
-
-/* 
-******************** TO DO ************************
-- voir aussi comment gerer l'affichage de chaque fichier .o .a .so ... selon le 'e_type' dans Ehdr (
-	surtout le cas des addresse en hexa decimal quand ils sont a zero, dans certains cas ils sont afficher ou pas
-)
-- Stocker les symbole dans la struct t_symbols (avant faire le tri)
-- Faire Tri
-
-*/
-
+	filename = (argc < 2) ? "a.out" : argv[1];
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("open");
+		return (1);
+	}
+	if (fstat(fd, &st) < 0)
+	{
+		perror("fstat");
+		close(fd);
+		return (1);
+	}
+	file_mem = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (file_mem == MAP_FAILED)
+	{
+		perror("mmap");
+		close(fd);
+		return (1);
+	}
+	ret = process_file(file_mem, &st, (char *)filename);
+	munmap(file_mem, st.st_size);
+	close(fd);
+	return (ret);
+}
